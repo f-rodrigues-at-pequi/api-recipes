48b9ce0ca6f606f076222249c745c2e4f3646313
Frederico Rodrigues <f.rodrigues@Fredericos-MacBook-Air.local>
2024-12-02 18:00:48 -0300

    fix cors

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import jsonServer from "json-server";
 const server = jsonServer.create()
 const router = jsonServer.router("db.json")
-
 const middlewares = jsonServer.defaults()
-
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 server.use(middlewares)
 server.use(
     jsonServer.rewriter({
