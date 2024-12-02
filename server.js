0f699577497789f7735a6f2d9ca013f1fe8db853
Frederico Rodrigues <f.rodrigues@Fredericos-MacBook-Air.local>
2024-12-02 18:06:58 -0300

    fix cors

--- server.js
+++ server.js
@@ -2,12 +2,7 @@ import jsonServer from "json-server";
 const server = jsonServer.create()
 const router = jsonServer.router("db.json")
 const middlewares = jsonServer.defaults()
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+
 server.use(middlewares)
 server.use(
     jsonServer.rewriter({
@@ -15,6 +10,10 @@ server.use(
     })
 )
 server.use(router)
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log("Server is running on port 3000")
