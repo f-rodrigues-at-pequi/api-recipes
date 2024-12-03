f0e2538c9f466c55f3c32c04fe04ae79af866784
Frederico Rodrigues <f.rodrigues@Fredericos-MacBook-Air.local>
2024-12-03 08:42:23 -0300

    change vercel json

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
 import jsonServer from "json-server";
-import cors from "cors";
-
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-
-
 server.use(middlewares);
 server.use(router);
-server.use(
-    cors({
-        origin: true,
-        credentials: true,
-        preflightContinue: false,
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    })
-);
-server.options("*", cors());
 server.listen(3001, () => {
     console.log("JSON Server is running");
 });
